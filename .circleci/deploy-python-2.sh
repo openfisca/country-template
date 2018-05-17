e1e45980749cabe3159395de768ee4da020cbbc1
Matti Schneider <labplus@matti.consulting>
2018-05-17 14:53:37 +1200

    Remove duplication in functional changes detection

--- .circleci/deploy-python-2.sh
+++ .circleci/deploy-python-2.sh
@@ -2,16 +2,13 @@
 
 set -e
 
-VERSION_CHANGE_TRIGGERS="setup.py MANIFEST.in openfisca_core openfisca_web_api_preview"
-
-if git diff-index --quiet HEAD^ -- $VERSION_CHANGE_TRIGGERS ":(exclude)*.md"
+if ./detect-functional-changes.sh
 then
+    git tag `python setup.py --version`
+    git push --tags  # update the repository version
+    python setup.py bdist_wheel  # build this package in the dist directory
+    twine upload dist/* --username $PYPI_USERNAME --password $PYPI_PASSWORD  # publish
+    ssh -o StrictHostKeyChecking=no deploy-api@fr.openfisca.org  # Deploy the OpenFisca-France public API
+else
     echo "No deployment - Only non-functional elements were modified in this change"
-    exit 0  # there are no changes at all, the version is correct
 fi
-
-git tag `python setup.py --version`
-git push --tags  # update the repository version
-python setup.py bdist_wheel  # build this package in the dist directory
-twine upload dist/* --username $PYPI_USERNAME --password $PYPI_PASSWORD  # publish
-ssh -o StrictHostKeyChecking=no deploy-api@fr.openfisca.org  # Deploy the OpenFisca-France public API
--- .circleci/deploy-python-3.sh
+++ .circleci/deploy-python-3.sh
@@ -2,14 +2,10 @@
 
 set -e
 
-VERSION_CHANGE_TRIGGERS="setup.py MANIFEST.in openfisca_core openfisca_web_api_preview"
-
-if git diff-index --quiet HEAD^ -- $VERSION_CHANGE_TRIGGERS ":(exclude)*.md"
+if ./detect-functional-changes.sh
 then
+    python setup.py bdist_wheel  # build this package in the dist directory
+    twine upload dist/* --username $PYPI_USERNAME --password $PYPI_PASSWORD  # publish
+else
     echo "No deployment - Only non-functional elements were modified in this change"
-    exit 0  # there are no changes at all, the version is correct
 fi
-
-
-python setup.py bdist_wheel  # build this package in the dist directory
-twine upload dist/* --username $PYPI_USERNAME --password $PYPI_PASSWORD  # publish
