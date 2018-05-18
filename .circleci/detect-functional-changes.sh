5846f05aeabba4eeb3f3bffd2e52dea59dbdf90e
Florian Pagnoux <florian.pagnoux@gmail.com>
2018-05-18 13:03:16 -0400

    Make script work in test and deploy context

--- .circleci/detect-functional-changes.sh
+++ .circleci/detect-functional-changes.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
 
 VERSION_CHANGE_TRIGGERS="setup.py MANIFEST.in openfisca_country_template"
+CURRENT_BRANCH=`git symbolic-ref --short HEAD`
 
-if git diff-index --quiet origin/master -- $VERSION_CHANGE_TRIGGERS ":(exclude)*.md"
+if [[ "$CURRENT_BRANCH" == "master" ]]
+then LAST_MASTER_VERSION="HEAD^"
+else LAST_MASTER_VERSION="origin/master"
+fi
+
+if git diff-index --quiet $LAST_MASTER_VERSION -- $VERSION_CHANGE_TRIGGERS ":(exclude)*.md"
 then echo "No functional changes detected."
 else exit 1
 fi
