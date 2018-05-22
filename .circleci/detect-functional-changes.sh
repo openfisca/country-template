b4285b89d2df3b70db9354cc874d124ec29cea4c
Matti Schneider <labplus@matti.consulting>
2018-05-22 12:54:28 +1200

    Return an exit code on functional changes detect

--- .circleci/detect-functional-changes.sh
+++ .circleci/detect-functional-changes.sh
@@ -4,7 +4,7 @@ VERSION_CHANGE_TRIGGERS="setup.py MANIFEST.in openfisca_country_template"
 
 last_tagged_commit=`git describe --tags --abbrev=0 --first-parent`  # --first-parent ensures we don't follow tags not published in master through an unlikely intermediary merge commit
 
-if git diff-index --shortstat $last_tagged_commit -- $VERSION_CHANGE_TRIGGERS ":(exclude)*.md"
+if git diff-index --shortstat --exit-code $last_tagged_commit -- $VERSION_CHANGE_TRIGGERS ":(exclude)*.md"
 then echo "No functional changes detected."
 else exit 1
 fi
