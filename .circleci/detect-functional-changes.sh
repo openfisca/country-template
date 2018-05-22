54b9bc4d20498c8c85c74220e1b801b6d7e1c696
Matti Schneider <labplus@matti.consulting>
2018-05-22 13:39:58 +1200

    Support older Git versions in change detection

--- .circleci/detect-functional-changes.sh
+++ .circleci/detect-functional-changes.sh
@@ -4,7 +4,7 @@ IGNORE_DIFF_ON="README.md CONTRIBUTING.md .gitignore .circleci/* .github/*"
 
 last_tagged_commit=`git describe --tags --abbrev=0 --first-parent`  # --first-parent ensures we don't follow tags not published in master through an unlikely intermediary merge commit
 
-if git diff-index --name-only --exit-code $last_tagged_commit -- `echo " $IGNORE_DIFF_ON" | sed 's/ / :(exclude)/g'`
+if git diff-index --name-only --exit-code $last_tagged_commit -- . `echo " $IGNORE_DIFF_ON" | sed 's/ / :(exclude)/g'`
 then echo "No functional changes detected."
 else
   echo "The functional files above were changed."
