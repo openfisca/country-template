cd5425c861b105e67a5ab7e37a8e74a30bdf2beb
Matti Schneider <labplus@matti.consulting>
2018-05-30 13:25:24 +1200

    Make bootstrap script portable

--- bootstrap.sh
+++ bootstrap.sh
@@ -30,11 +30,16 @@ git init
 git add .
 git commit --message 'Initial import from OpenFisca country-template' --author='OpenFisca Bot <bot@openfisca.org>'
 
-sed -i -e "3,${last_bootstrapping_line_number}d" README.md  # remove instructions lines
-sed -i "s|country_template|$lowercase_country_name|g" README.md setup.py .circleci/config.yml Makefile `find openfisca_country_template -type f`
-sed -i "s|country-template|$lowercase_country_name|g" README.md
-sed -i "s|Country-Template|$COUNTRY_NAME|g" README.md setup.py .github/PULL_REQUEST_TEMPLATE.md CONTRIBUTING.md `find openfisca_country_template -type f`
-sed -i "s|https://github.com/openfisca/openfisca-country-template|$URL|g" setup.py
+all_module_files=`find openfisca_country_template -type f`
+
+# Use intermediate backup files due to lack of portable 'no backup' option. See https://stackoverflow.com/q/5694228/594053
+sed -i '.template' "s|country_template|$lowercase_country_name|g" README.md setup.py .circleci/config.yml Makefile $all_module_files
+sed -i '.template' "s|Country-Template|$COUNTRY_NAME|g" README.md setup.py .github/PULL_REQUEST_TEMPLATE.md CONTRIBUTING.md $all_module_files
+sed -i '.template' -e "3,${last_bootstrapping_line_number}d" README.md  # remove instructions lines
+sed -i '.template' "s|country-template|$lowercase_country_name|g" README.md
+sed -i '.template' "s|https://github.com/openfisca/openfisca-country-template|$URL|g" setup.py
+find . -name "*.template" -type f -delete
+
 git mv openfisca_country_template openfisca_$lowercase_country_name
 
 git rm bootstrap.sh
