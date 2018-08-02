0aa383ea4d65ee20faff3e7a841cc291a53acd90
Florian Pagnoux <florian.pagnoux@gmail.com>
2018-08-01 21:51:46 -0400

    Install OpenFisca Web API by default

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
     url='https://github.com/openfisca/country-template',
     include_package_data = True,  # Will read MANIFEST.in
     install_requires=[
-        'OpenFisca-Core >= 23.3, < 24.0',
+        'OpenFisca-Core[web-api] >= 24.0, < 25.0',
         ],
     extras_require = {
         'test': [
