b621c01b1ca891cc457e027c1d51588a7d86b3d3
Florian Pagnoux <florian.pagnoux@gmail.com>
2018-08-01 21:51:46 -0400

    Remove obsolete extra require

--- setup.py
+++ setup.py
@@ -19,9 +19,6 @@ setup(
         'OpenFisca-Core >= 23.3, < 24.0',
         ],
     extras_require = {
-        'api': [
-            'OpenFisca-Web-API >= 4.0.0, < 7.0',
-            ],
         'test': [
             'flake8 >= 3.4.0, < 3.5.0',
             'flake8-print',
