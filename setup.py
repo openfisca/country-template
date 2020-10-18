78e335221bc50f66359bbd2642851d2c6a444d09
Mauko Quiroga <mauko.quiroga@data.gouv.fr>
2020-10-18 21:36:04 +0200

    Add flake8-comprehensions

--- setup.py
+++ setup.py
@@ -35,6 +35,7 @@ setup(
             "flake8-bugbear >= 20.1.0, < 21.0.0",
             "flake8-coding >= 1.3.0, < 2.0.0",
             "flake8-commas >= 2.0.0, < 3.0.0",
+            "flake8-comprehensions >= 3.2.0, < 4.0.0",
             "flake8-docstrings >= 1.5.0, < 2.0.0",
             "flake8-import-order >= 0.18.0, < 1.0.0",
             "flake8-print >= 3.1.0, < 4.0.0",
