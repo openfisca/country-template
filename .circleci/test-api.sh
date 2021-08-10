c0d7269a0f41553472cf50d44c85f4a97644d4b5
HAEKADI <hajar.aitelkadi.o@gmail.com>
2021-08-10 15:20:56 +0200

    Correct endpoint

--- .circleci/test-api.sh
+++ .circleci/test-api.sh
@@ -6,7 +6,7 @@ apt-get install -y jq
 make serve-local &
 
 PORT=5000
-ENDPOINT=specs # wrong, to test the --fail option
+ENDPOINT=spec
 
 response=$(curl --retry-connrefused --retry 10 --retry-delay 5 --fail http://127.0.0.1:$PORT/$ENDPOINT | jq -e)
 
