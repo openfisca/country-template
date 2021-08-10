247f947c4b2f5274332d25d14f4b92565f6cc298
HAEKADI <hajar.aitelkadi.o@gmail.com>
2021-08-10 12:37:31 +0200

    Refactor script

--- .circleci/test-api.sh
+++ .circleci/test-api.sh
@@ -5,31 +5,8 @@ apt-get install -y jq
 
 make serve-local &
 
-curl --retry-connrefused \
-    --retry 5 \
-    --retry-delay 20 \
-    http://127.0.0.1:5000 --fail
+PORT=5000
+ENDPOINT=spec
 
-var=$?
-
-if [[ $var -ne 0 ]]
-then
-  echo "Server failed to start."
-  exit $var
-fi
-
-response=$(curl --write-out '%{http_code}' --silent --output /dev/null http://127.0.0.1:5000/spec)
-if [[ $response -ne 200 ]]
-then
-  echo "Accessing the swagger endpoint returns an HTTP $response code instead of a 200 code."
-  exit 1
-fi
-
-curl http://127.0.0.1:5000/spec --fail | jq -e
-set var=$?
-
-if [[ $var -ne 0 ]]
-then
-  echo "The resulting swagger endpoint is not a valid JSON."
-  exit $var
-fi
+set -e
+curl --retry-connrefused --retry 10 --retry-delay 5 --fail http://127.0.0.1:$PORT/$ENDPOINT | jq -e
