ec355b647206ce9a2132067dd7fdbd5c73a92524
HAEKADI <hajar.aitelkadi.o@gmail.com>
2021-08-11 17:55:25 +0200

    Switch to openfisca serve

--- .circleci/test-api.sh
+++ .circleci/test-api.sh
@@ -1,8 +1,8 @@
 #! /usr/bin/env bash
 
-make serve-local &
-
 PORT=5000
 ENDPOINT=spec
+
+openfisca serve --country-package openfisca_country_template --port $PORT &
 
 curl --retry-connrefused --retry 10 --retry-delay 5 --fail http://127.0.0.1:$PORT/$ENDPOINT | python -m json.tool > /dev/nul
