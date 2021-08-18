6f6bbbd0425ed98b01a33833696f64632d86a95b
Matti Schneider <matti.schneider@beta.gouv.fr>
2021-08-18 17:26:02 +0200

    Fix typos

--- .circleci/test-api.sh
+++ .circleci/test-api.sh
@@ -5,4 +5,4 @@ ENDPOINT=spec
 
 openfisca serve --country-package openfisca_country_template --port $PORT &
 
-curl --retry-connrefused --retry 10 --retry-delay 5 --fail http://127.0.0.1:$PORT/$ENDPOINT | python -m json.tool > /dev/nul
+curl --retry-connrefused --retry 10 --retry-delay 5 --fail http://127.0.0.1:$PORT/$ENDPOINT | python -m json.tool > /dev/null
