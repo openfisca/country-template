9325949a0738d5e62f8e47c80813bf706568f93c
Matti Schneider <matti@openfisca.org>
2024-04-24 17:20:25 +0545

    Update references to setup.py in CI workflow

--- .github/is-version-number-acceptable.sh
+++ .github/is-version-number-acceptable.sh
@@ -12,14 +12,14 @@ then
     exit 0
 fi
 
-current_version=$(python setup.py --version)
+current_version=$(grep '^version =' pyproject.toml | cut -d '"' -f 2)  # parsing with tomllib is complicated, see https://github.com/python-poetry/poetry/issues/273
 
 if git rev-parse --verify --quiet $current_version
 then
     echo "Version $current_version already exists in commit:"
     git --no-pager log -1 $current_version
     echo
-    echo "Update the version number in setup.py before merging this branch into main."
+    echo "Update the version number in pyproject.toml before merging this branch into main."
     echo "Look at the CONTRIBUTING.md file to learn how the version number should be updated."
     exit 1
 fi
