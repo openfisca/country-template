2782d3a8f2cc546e647054c37d3469f04a7d14e6
Nicolas Dupont <npg.dupont@gmail.com>
2024-05-09 17:14:58 +0200

    Ensure scripts fail if finding the latest tagged commit fails

--- .github/lint-changed-yaml-tests.sh
+++ .github/lint-changed-yaml-tests.sh
@@ -2,6 +2,11 @@
 
 last_tagged_commit=`git describe --tags --abbrev=0 --first-parent`  # --first-parent ensures we don't follow tags not published in main through an unlikely intermediary merge commit
 
+if [ $? -ne 0 ]; then
+    echo "Error: Failed to find the last tagged commit."
+    exit 1
+fi
+
 if ! changed_files=$(git diff-index --name-only --diff-filter=ACMR --exit-code $last_tagged_commit -- "tests/*.yaml")
 then
   echo "Linting the following changed YAML tests:"
