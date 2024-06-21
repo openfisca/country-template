228af546533b0ee5bf155449d68e06721a2b8c48
Matti Schneider <matti@openfisca.org>
2024-06-21 13:45:13 +0200

    Validate changelog with changelog-action/validate

--- .github/is-version-number-acceptable.sh
+++ .github/is-version-number-acceptable.sh
@@ -1,11 +1,5 @@
 #! /usr/bin/env bash
 
-if ! $(dirname "$BASH_SOURCE")/has-functional-changes.sh
-then
-    echo "No need for a version update."
-    exit 0
-fi
-
 current_version=$(grep '^version =' pyproject.toml | cut -d '"' -f 2)  # parsing with tomllib is complicated, see https://github.com/python-poetry/poetry/issues/273
 
 if [[ ! $current_version ]]
@@ -21,13 +15,5 @@ then
     echo
     echo "Update the version number in pyproject.toml before merging this branch into main."
     echo "Look at the CONTRIBUTING.md file to learn how the version number should be updated."
-    exit 2
-fi
-
-if ! $(dirname "$BASH_SOURCE")/has-functional-changes.sh | grep --quiet CHANGELOG.md
-then
-    echo "CHANGELOG.md has not been modified, while functional changes were made."
-    echo "Explain what you changed before merging this branch into main."
-    echo "Look at the CONTRIBUTING.md file to learn how to write the changelog."
     exit 2
 fi
