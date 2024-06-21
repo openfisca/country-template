6c07f983639c9b6e46966a2db61630b70977dd16
Matti Schneider <matti@openfisca.org>
2024-06-21 13:45:51 +0200

    Update changelog with changelog-action/release

--- .github/publish-git-tag.sh
+++
@@ -1,5 +0,0 @@
-#! /usr/bin/env bash
-
-current_version=$(grep '^version =' pyproject.toml | cut -d '"' -f 2)  # parsing with tomllib is complicated, see https://github.com/python-poetry/poetry/issues/273
-git tag $current_version
-git push --tags  # update the repository version
