b8131ffbfb5da3eb746862ae9ea59e46769d1959
Mauko Quiroga <public@mauko.me>
2024-10-14 16:20:13 +0200

    build: normalize pyproject

--- openfisca_country_template/variables/benefits.py
+++ openfisca_country_template/variables/benefits.py
@@ -1,12 +1,11 @@
-"""
-This file defines variables for the modelled legislation.
+"""This file defines variables for the modelled legislation.
 
 A variable is a property of an Entity such as a Person, a Household…
 
 See https://openfisca.org/doc/key-concepts/variables.html
 """
 
-# Import from openfisca-core the Python objects used to code the legislation in OpenFisca
+# Import from openfisca-core the objects used to code the legislation in OpenFisca
 from openfisca_core.periods import MONTH
 from openfisca_core.variables import Variable
 
@@ -19,27 +18,39 @@ class basic_income(Variable):
     entity = Person
     definition_period = MONTH
     label = "Basic income provided to adults"
-    reference = "https://law.gov.example/basic_income"  # Always use the most official source
+    reference = (
+        "https://law.gov.example/basic_income"  # Always use the most official source
+    )
 
     def formula_2016_12(person, period, parameters):
-        """
-        Basic income provided to adults.
+        """Basic income provided to adults.
 
-        Since Dec 1st 2016, the basic income is provided to any adult, without considering their income.
+        Since Dec 1st 2016, the basic income is provided to any adult, without
+        considering their income.
         """
-        age_condition = person("age", period) >= parameters(period).general.age_of_majority
-        return age_condition * parameters(period).benefits.basic_income  # This '*' is a vectorial 'if'. See https://openfisca.org/doc/coding-the-legislation/25_vectorial_computing.html#control-structures
+        age_condition = (
+            person("age", period) >= parameters(period).general.age_of_majority
+        )
+        # This '*' is a vectorial 'if'. See
+        # https://openfisca.org/doc/coding-the-legislation/25_vectorial_computing.html#control-structures
+        return age_condition * parameters(period).benefits.basic_income
 
     def formula_2015_12(person, period, parameters):
-        """
-        Basic income provided to adults.
+        """Basic income provided to adults.
 
-        From Dec 1st 2015 to Nov 30 2016, the basic income is provided to adults who have no income.
-        Before Dec 1st 2015, the basic income does not exist in the law, and calculating it returns its default value, which is 0.
+        From Dec 1st 2015 to Nov 30 2016, the basic income is provided to adults
+        who have no income. Before Dec 1st 2015, the basic income does not exist
+        in the law, and calculating it returns its default value, which is 0.
         """
-        age_condition = person("age", period) >= parameters(period).general.age_of_majority
+        age_condition = (
+            person("age", period) >= parameters(period).general.age_of_majority
+        )
         salary_condition = person("salary", period) == 0
-        return age_condition * salary_condition * parameters(period).benefits.basic_income  # The '*' is also used as a vectorial 'and'. See https://openfisca.org/doc/coding-the-legislation/25_vectorial_computing.html#boolean-operations
+        # The '*' is also used as a vectorial 'and'. See
+        # https://openfisca.org/doc/coding-the-legislation/25_vectorial_computing.html#boolean-operations
+        return (
+            age_condition * salary_condition * parameters(period).benefits.basic_income
+        )
 
 
 class housing_allowance(Variable):
@@ -47,8 +58,11 @@ class housing_allowance(Variable):
     entity = Household
     definition_period = MONTH
     label = "Housing allowance"
-    reference = "https://law.gov.example/housing_allowance"  # Always use the most official source
-    end = "2016-11-30"  # This allowance was removed on the 1st of Dec 2016. Calculating it before this date will always return the variable default value, 0.
+    # Always use the most official source
+    reference = "https://law.gov.example/housing_allowance"
+    # This allowance was removed on the 1st of Dec 2016. Calculating it before
+    # this date will always return the variable default value, 0.
+    end = "2016-11-30"
     unit = "currency-EUR"
     documentation = """
     This allowance was introduced on the 1st of Jan 1980.
@@ -56,36 +70,37 @@ class housing_allowance(Variable):
     """
 
     def formula_1980(household, period, parameters):
-        """
-        Housing allowance.
+        """Housing allowance.
 
-        This allowance was introduced on the 1st of Jan 1980.
-        Calculating it before this date will always return the variable default value, 0.
+        This allowance was introduced on the 1st of Jan 1980. Calculating it
+        before this date will always return the variable default value, 0.
 
-        To compute this allowance, the 'rent' value must be provided for the same month,
-        but 'housing_occupancy_status' is not necessary.
+        To compute this allowance, the 'rent' value must be provided for the
+        same month, but 'housing_occupancy_status' is not necessary.
         """
         return household("rent", period) * parameters(period).benefits.housing_allowance
 
 
-# By default, you can use utf-8 characters in a variable. OpenFisca web API manages utf-8 encoding.
+# By default, you can use utf-8 characters in a variable. OpenFisca web API
+# manages utf-8 encoding.
 class pension(Variable):
     value_type = float
     entity = Person
     definition_period = MONTH
     label = "Pension for the elderly. Pension attribuée aux personnes âgées. تقاعد."
-    reference = ["https://fr.wikipedia.org/wiki/Retraite_(économie)", "https://ar.wikipedia.org/wiki/تقاعد"]
+    reference = (
+        "https://fr.wikipedia.org/wiki/Retraite_(économie)",
+        "https://ar.wikipedia.org/wiki/تقاعد",
+    )
 
     def formula(person, period, parameters):
-        """
-        Pension for the elderly.
+        """Pension for the elderly.
 
         A person's pension depends on their birth date.
         In French: retraite selon l'âge.
         In Arabic: تقاعد.
         """
-        age_condition = person("age", period) >= parameters(period).general.age_of_retirement
-        return age_condition
+        return person("age", period) >= parameters(period).general.age_of_retirement
 
 
 class parenting_allowance(Variable):
@@ -97,8 +112,7 @@ class parenting_allowance(Variable):
     reference = "https://www.servicesaustralia.gov.au/individuals/services/centrelink/parenting-payment/who-can-get-it"
 
     def formula(household, period, parameters):
-        """
-        Parenting allowance for households.
+        """Parenting allowance for households.
 
         A person's parenting allowance depends on how many dependents they have,
         how much they, and their partner, earn
--- openfisca_country_template/variables/taxes.py
+++ openfisca_country_template/variables/taxes.py
@@ -1,13 +1,12 @@
-"""
-This file defines variables for the modelled legislation.
+"""This file defines variables for the modelled legislation.
 
 A variable is a property of an Entity such as a Person, a Household…
 
 See https://openfisca.org/doc/key-concepts/variables.html
 """
 
-# Import from numpy the operations you need to apply on OpenFisca's population vectors
-# Import from openfisca-core the Python objects used to code the legislation in OpenFisca
+# Import from numpy the what you need to apply on OpenFisca's population vectors
+# Import from openfisca-core the objects used to code the legislation in OpenFisca
 from numpy import maximum as max_
 
 from openfisca_core.periods import MONTH, YEAR
@@ -22,11 +21,12 @@ class income_tax(Variable):
     entity = Person
     definition_period = MONTH
     label = "Income tax"
-    reference = "https://law.gov.example/income_tax"  # Always use the most official source
+    reference = (
+        "https://law.gov.example/income_tax"  # Always use the most official source
+    )
 
     def formula(person, period, parameters):
-        """
-        Income tax.
+        """Income tax.
 
         The formula to compute the income tax for a given person at a given period
         """
@@ -38,11 +38,11 @@ class social_security_contribution(Variable):
     entity = Person
     definition_period = MONTH
     label = "Progressive contribution paid on salaries to finance social security"
-    reference = "https://law.gov.example/social_security_contribution"  # Always use the most official source
+    # Always use the most official source
+    reference = "https://law.gov.example/social_security_contribution"
 
     def formula(person, period, parameters):
-        """
-        Social security contribution.
+        """Social security contribution.
 
         The social_security_contribution is computed according to a marginal scale.
         """
@@ -57,25 +57,32 @@ class housing_tax(Variable):
     entity = Household
     definition_period = YEAR  # This housing tax is defined for a year.
     label = "Tax paid by each household proportionally to the size of its accommodation"
-    reference = "https://law.gov.example/housing_tax"  # Always use the most official source
+    reference = (
+        "https://law.gov.example/housing_tax"  # Always use the most official source
+    )
 
     def formula(household, period, parameters):
-        """
-        Housing tax.
+        """Housing tax.
 
-        The housing tax is defined for a year, but depends on the `accommodation_size` and `housing_occupancy_status` on the first month of the year.
-        Here period is a year. We can get the first month of a year with the following shortcut.
-        To build different periods, see https://openfisca.org/doc/coding-the-legislation/35_periods.html#calculate-dependencies-for-a-specific-period
+        The housing tax is defined for a year, but depends on the `accommodation_size`
+        and `housing_occupancy_status` on the first month of the year. Here period
+        is a year. We can get the first month of a year with the following shortcut.
+        To build different periods, see
+        https://openfisca.org/doc/coding-the-legislation/35_periods.html#calculate-dependencies-for-a-specific-period
         """
         january = period.first_month
         accommodation_size = household("accommodation_size", january)
 
         tax_params = parameters(period).taxes.housing_tax
-        tax_amount = max_(accommodation_size * tax_params.rate, tax_params.minimal_amount)
+        tax_amount = max_(
+            accommodation_size * tax_params.rate, tax_params.minimal_amount
+        )
 
         # `housing_occupancy_status` is an Enum variable
         occupancy_status = household("housing_occupancy_status", january)
-        HousingOccupancyStatus = occupancy_status.possible_values  # Get the enum associated with the variable
+        HousingOccupancyStatus = (
+            occupancy_status.possible_values
+        )  # Get the enum associated with the variable
         # To access an enum element, we use the `.` notation.
         tenant = occupancy_status == HousingOccupancyStatus.tenant
         owner = occupancy_status == HousingOccupancyStatus.owner
