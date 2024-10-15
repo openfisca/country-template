c059c3d009eb33846ca588ba20e1d4a0d0191917
Mauko Quiroga <public@mauko.me>
2024-10-15 14:18:25 +0200

    ci: build conda pkg

--- .github/is-version-number-acceptable.sh
+++ .github/is-version-number-acceptable.sh
@@ -12,18 +12,29 @@ then
     exit 0
 fi
 
-current_version=$(grep '^version =' pyproject.toml | cut -d '"' -f 2)  # parsing with tomllib is complicated, see https://github.com/python-poetry/poetry/issues/273
+# parsing with tomllib is complicated, see
+# https://github.com/python-poetry/poetry/issues/273
+python_version=$(grep '^version =' pyproject.toml | cut -d '"' -f 2)
 
-if [[ ! $current_version ]]
+# we do the same for conda
+conda_version=$(grep '^  version: \d' .conda/recipe.yaml | cut -d ' ' -f 4)
+
+if [[ ! $python_version || ! $conda_version ]]
 then
     echo "Error getting current version"
     exit 1
 fi
 
-if git rev-parse --verify --quiet $current_version
+if [[ "$python_version" != "$conda_version" ]]
 then
-    echo "Version $current_version already exists in commit:"
-    git --no-pager log -1 $current_version
+    echo "Python ($python_version) and Conda ($conda_version) versions do not match"
+    exit 1
+fi
+
+if git rev-parse --verify --quiet "$python_version"
+then
+    echo "Version $python_version already exists in commit:"
+    git --no-pager log -1 "$python_version"
     echo
     echo "Update the version number in pyproject.toml before merging this branch into main."
     echo "Look at the CONTRIBUTING.md file to learn how the version number should be updated."
