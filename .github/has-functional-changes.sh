dcc9569bb16c3529cb79c3ade2ac6b843505794a
Thomas Guillet <thomas@palya.fr>
2026-09-16 20:09:14 +0200

    Improve has-functional-changes robustness

--- .github/has-functional-changes.sh
+++ .github/has-functional-changes.sh
@@ -2,6 +2,13 @@
 
 IGNORE_DIFF_ON="README.md CONTRIBUTING.md Makefile .gitignore .github/*"
 
+tag_count=`git tag | wc -l`
+if [[ $tag_count -eq 0 ]]
+then
+  echo "No tags to compare revision."
+  exit 0
+fi
+
 last_tagged_commit=`git describe --tags --abbrev=0 --first-parent`  # --first-parent ensures we don't follow tags not published in main through an unlikely intermediary merge commit
 
 if git diff-index --name-only --exit-code $last_tagged_commit -- . `echo " $IGNORE_DIFF_ON" | sed 's/ / :(exclude)/g'`  # Check if any file that has not be listed in IGNORE_DIFF_ON has changed since the last tag was published.
